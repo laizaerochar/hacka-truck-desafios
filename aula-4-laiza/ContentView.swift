e3848908fcb82d5d3f297dc3a4c422490b5f506a
Ruan <ruanpablooliveira20@gmail.com>
2026-04-22 18:15:27 -0300

    Initial Commit

---
+++ aula-4-laiza/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  aula-4-laiza
+//
+//  Created by Turma01-11 on 22/04/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ aula-4-laiza/aula_4_laizaApp.swift
@@ -0,0 +1,17 @@
+//
+//  aula_4_laizaApp.swift
+//  aula-4-laiza
+//
+//  Created by Turma01-11 on 22/04/26.
+//
+
+import SwiftUI
+
+@main
+struct aula_4_laizaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
